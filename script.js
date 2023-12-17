d9000cc6cfc7ba59103455347c0e708d2d934006
DanielB566 <Danib7382@gmail.com>
2023-12-16 20:54:42 -0500

    Added background/images made images change depending on message

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function shakeMagic8Ball() {
   if (question === null) {
     document.getElementById("response-text").innerText =
       "Please ask a question!";
-    document.getElementById("response-text").classList = "display text-danger";
-    document.getElementById("reponse-img").src = "#replac/ewithbadimg"; //fix later//
+    document.getElementById("response-text").classList = "display text-danger ";
+    document.getElementById("reponse-img").src = "imgs/noo.png"; //fix later//
   }
 
   if (!question.trim()) {
@@ -18,41 +18,49 @@ function shakeMagic8Ball() {
   switch (randomNumber) {
     case 0:
       answer = "Yes";
-      image = "yes-image.jpg"; //Change this;
+      document.getElementById("reponse-img").src = "imgs/yess.png"; //fix later//
+      image = "imgs/yess.png"; //Change this;
       color = "text-success";
       break;
     case 1:
       answer = "No";
+      document.getElementById("reponse-img").src = "imgs/noo.png"; //fix later//
       image = "no-image.jpg"; //Change this;
       color = "text-danger";
       break;
     case 2:
       answer = "It is certain";
+      document.getElementById("reponse-img").src = "imgs/yess.png"; //fix later//
       image = "It is certain-image.jpg"; //Change this;
       color = "text-success";
       break;
     case 3:
       answer = "It is decidely so";
+      document.getElementById("reponse-img").src = "imgs/yess.png"; //fix later//
       image = "It is decidely so-image.jpg"; //Change this;
       color = "text-success";
       break;
     case 4:
     answer = "cannot predict now";
+    document.getElementById("reponse-img").src = "imgs/puzzled.png"; //fix later//
      image = "cannot predict now-image.jpg"; //Change this;
      color = "text-muted";
      break;
     case 5:
     answer = "Do not count on it";
+    document.getElementById("reponse-img").src = "imgs/noo.png"; //fix later//
     image = "Do not count on it-image.jpg"; //Change this;
     color = "text-danger";
     break;
     case 6:
     answer = "My sources say no";
+    document.getElementById("reponse-img").src = "imgs/noo.png"; //fix later//
     image = "My sources say no-image.jpg"; //Change this;
     color = "text-danger";
     break;
     case 7:
     answer = "Signs point to yes";
+    document.getElementById("reponse-img").src = "imgs/yess.png"; //fix later//
     image = "Signs point to yes-image.jpg"; //Change this;
     color = "text-success";
     break;
@@ -62,6 +70,6 @@ const responseText = document.getElementById('response-text');
 responseText.innerText = answer;
 responseText.classList = `display-4 ${color}`;
 document.getElementById('response-image').src = image;
-
+document.getElementById('response-image').src = `images/${image}`
 
 }
